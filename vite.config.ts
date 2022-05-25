9dbf344ae909ac12468a5f979969caf7a16d79e1
Adrian Czarnous <adrian.czarnous@infermedica.com>
2022-05-26 01:19:00 +0200

    auto imports config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,23 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), AutoImport({
+    dts: './auto-imports.d.ts',
+    include: [
+      /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+      /\.vue$/, /\.vue\?vue/, // .vue
+      /\.md$/, // .md
+    ],
+    imports: [
+      'vue',
+      'vue-router',
+    ]
+  })
+      ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
