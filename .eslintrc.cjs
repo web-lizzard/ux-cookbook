50be4a9b8d6eb7ac9e8b73d73719112dd1361793
Adrian Czarnous <adrian.czarnous@infermedica.com>
2022-05-26 01:37:26 +0200

    add auto import deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,11 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   "root": true,
   "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended"
-  ],
+      "plugin:vue/vue3-essential",
+      "eslint:recommended",
+      "@vue/eslint-config-typescript/recommended",
+      "plugin:storybook/recommended"],
   "env": {
     "vue/setup-compiler-macros": true
   }
-}
+};
--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,4 @@
+const AutoImport = require("unplugin-auto-import/vite");
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -14,5 +15,17 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
-  }
+  },
+    async viteFinal(config, { configType }) {
+      config.plugins = config.plugins ?? []
+      config.plugins.push(AutoImport({
+        imports: [
+          'vue',
+          'vue-router',
+        ],
+        dts: '.storybook/auto-imports.d.ts',
+      }))
+
+      return config
+  },
 }
